cf4b64e04ad8f903e7feca59635896ac2a76385c
Melsara <kaxhortz@gmail.com>
2018-02-28 20:52:26 +0100

    Introducing ArrayAdapter and ListView.

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -18,7 +18,9 @@ package com.example.android.miwok;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -42,13 +44,11 @@ public class NumbersActivity extends AppCompatActivity {
         engNumbers.add(getString(R.string.eng_9));
         engNumbers.add(getString(R.string.eng_10));
 
-        LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, engNumbers);
 
-        for ( int i=0; i<engNumbers.size(); i++) {
-            TextView numberView = new TextView (this);
-            rootView.addView(numberView);
-            numberView.setText(engNumbers.get(i));
-        }
+        ListView listView = (ListView) findViewById(R.id.list);
+
+        listView.setAdapter(itemsAdapter);
 
 
 
