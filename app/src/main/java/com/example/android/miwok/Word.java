245136589eb623838c8592571c5f9013ad4563fa
Melsara <kaxhortz@gmail.com>
2018-03-02 21:17:45 +0100

    Fixing order of miwok-default words.

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -8,9 +8,9 @@ public class Word {
     private String mDefaultTranslation;
     private String mMiwokTranslation;
 
-    public Word (String defaultTranslation, String miwokTranslation){
-        mDefaultTranslation = defaultTranslation;
+    public Word (String miwokTranslation, String defaultTranslation){
         mMiwokTranslation = miwokTranslation;
+        mDefaultTranslation = defaultTranslation;
     }
 
     public String getDefaultTranslation() {
